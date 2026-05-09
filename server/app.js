041b839e7b31296b77c58bf06558f320e0288645
Ashish Kumar Mishra <amishrahsm@gmail.com>
2026-05-09 11:16:26 +0530

    Fix Railway serve deployment

--- server/app.js
+++ server/app.js
@@ -55,15 +55,7 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/messages', messageRoutes);
 
 
-if (process.env.NODE_ENV === 'production') {
- 
-  app.use(express.static(path.join(__dirname, 'public')));
-
-  app.get('*', (req, res) => {
-    res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
-  });
-}
+// Independent deployment: Static serving removed
 
 
 app.use(errorHandler);
